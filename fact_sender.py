f15c42bdd43ed3096a319f2d578c5830bf66d01e
lucas <lub21@pitt.edu>
2025-02-16 23:04:27 -0500

    add some basic error handling

--- fact_sender.py
+++ fact_sender.py
@@ -4,21 +4,21 @@ from pushover import Pushover
 
 def main():
     supabase: Client = create_client("https://urpytqjjpixevrhbzkvx.supabase.co", Constants.SUPABASE_API_KEY)
+    po = Pushover(Constants.PUSHOVER_APP_TOKEN)
+    po.user(Constants.PUSHOVER_USER_TOKEN)
     response = supabase.rpc("get_random_fact", params={}).execute()
+    if not response.data:
+        empty_msg = po.msg("We have run out of facts! Resetting db and will return tomorrow.")
+        empty_msg.set("title", "uh oh")
+        po.send(empty_msg)
+        exit(0)
     fact = response.data[0]
     fact_id = fact.get('id')
     fact_text = fact.get('fact_text')
-    po = Pushover(Constants.PUSHOVER_APP_TOKEN)
-    po.user(Constants.PUSHOVER_USER_TOKEN)
-
     msg = po.msg(fact_text)
     msg.set("title", "Football Fact of the Day")
     po.send(msg)
-
-    # supabase.table("football_facts").update({"sent_status": "true"}) .eq("id", fact_id).execute()
-
-
-
+    supabase.table("football_facts").update({"sent_status": "true"}).eq("id", fact_id).execute()
 
 # Call the main function if this script is run directly
 if __name__ == "__main__":
